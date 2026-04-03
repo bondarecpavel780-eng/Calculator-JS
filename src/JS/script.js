8a6b7446c4535511d2e25e82a085a1579eece0b5
Pavel <bondarecpavel780@gmail.com>
2026-04-03 20:30:45 +0300

    update calculator and add new function and buttons

--- src/JS/script.js
+++ src/JS/script.js
@@ -1,13 +1,31 @@
 "use strict";
 
-const display = document.querySelector(".text"),
+  const display = document.querySelector(".text"),
   btns = document.querySelectorAll(".panel button");
 
 btns.forEach((button) => {
   button.addEventListener("click", () => {
     if (button.id == "del") {
       display.value = display.value.slice(0, -1);
-    } else if (button.id === "result") {
+    }
+    else if (button.id === "AC") {
+      display.value = "";
+    }
+    else if (button.id === "procent") {
+      try {
+        if (display.value) {
+          let res = new Function("return " + display.value)();
+          if (res === Infinity || res === -Infinity || Number.isNaN(res)) {
+            display.value = "Ошибка";
+          } else {
+            display.value = (res / 100);
+          }
+        }
+      } catch (error) {
+        display.value = "Error";
+      }
+    }
+    else if (button.id === "result") {
       try {
         if (display.value) {
           let res = new Function("return " + display.value)();
@@ -28,3 +46,15 @@ btns.forEach((button) => {
     }
   });
 });
+
+const switcher = document.querySelector('.theme');
+
+  switcher.addEventListener('click', () => {
+  document.body.classList.toggle('dark');
+
+});
+
+const end = document.querySelector('.exit');
+end.addEventListener('click', ()=>{
+  close();
+})
